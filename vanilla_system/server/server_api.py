c39119daeeae6aaef50cf879c0ef830f134818cd
Thanh Nhan <99491940+LNThanhNhan@users.noreply.github.com>
2024-08-01 22:15:00 +0700

    Remove code

--- vanilla_system/server/server_api.py
+++ vanilla_system/server/server_api.py
@@ -1,16 +1,5 @@
-# File contains all api methods for server
-
-from fastapi import FastAPI
+#Launches the server
 from server_api_class import ServerApi
-     
-server=FastAPI()
+
 control = ServerApi()
 control.launch_fl_session()
-
-@server.post("/config")
-async def config():
-    return
-
-@server.post("/launchFL")
-async def launch_fl():
-    return control.launch_fl_session()           
